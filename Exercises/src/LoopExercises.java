b71b98f5d5a922cd04e0c8aa756b4b3c8ada14b1
Mohammed Omer <mohammedgomer@outlook.com>
2020-07-15 17:01:51 +0100

    Exercises commit

---
+++ Exercises/src/LoopExercises.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LoopExercises {
+
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+		
+//		for(int i = 1; i<=12;i++) {
+//			System.out.println(i + " x 6 = " + i*6);
+//		}
+	
+		Scanner sc = new Scanner(System.in);
+		for()
+	
+	
+	
+	
+	
+	}
+}
