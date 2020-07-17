933f31bd8ada7f36fd1819bed1f75b2a4603abbf
Mohammed Omer <mohammedgomer@outlook.com>
2020-07-17 22:42:30 +0100

    Update

--- Types/src/MortgageCalculator.java
+++ Types/src/MortgageCalculator.java
@@ -5,88 +5,69 @@ public class MortgageCalculator {
 
 	public static void main(String[] args) {
 
-		// Constants - As months in a year and percentage never change
-		final byte MONTHS_IN_YEAR = 12;
-		final byte PERCENT = 100;
-
 		// Scanner to get user input
 		Scanner sc = new Scanner(System.in);
 
-		// Principle - Amount of loan we want to get
+		int principle = (int) readNumber("Principle: ", 1000, 1000000);
 
-//		System.out.println("Principle £1K to £1M: ");
-//		int principle = sc.nextInt(); 
-//		while((principle<1000) || (principle > 1_000_000)) {
-//			System.out.println("Enter a value between £1K to £1M");
-//			continue;
-//		}
+		double interestRate = readNumber("Annual Interest Rate: ", 1, 30);
 
-		int principle;
-		while (true) {
-			System.out.println("Principle £1K to £1M: ");
-			principle = sc.nextInt();
-			if ((principle < 1000) || (principle > 1_000_000)) {
-				System.out.println("Enter a number between 1,000 and 1,000,000.");
-				continue;
-			} else {
-				break;
-			}
-		}
+		int period = (int) readNumber("Period (Years): ", 1, 30);
 
-		// Annual Interest Rate
-		double interestRate;
-		while (true) {
-			System.out.print("Annual Interest Rate: ");
-			interestRate = sc.nextDouble();
-			if ((interestRate) <= 0 || (interestRate > 30)) {
-				System.out.println("Enter a value greater than 0 and less than or equal to 30.");
-				continue;
-			} else {
-				break;
-			}
-		}
-
-		// Period (Years) - Loan for 30 years
-		int period;
-		while (true) {
-			System.out.print("Period (Years): ");
-			period = sc.nextInt();
-			if ((period <= 0) || period > 30) {
-				System.out.println("Enter a value between 1 and 30.");
-				continue;
-			} else {
-				break;
-			}
-		}
+		double mortgage = calculateMortgage(principle, interestRate, period);
 
 		// Formatting the mortgage value into currency so that it is readable, method
 		// chaining instead
+		System.out.println();
+		System.out.println("MORTGAGE");
+		System.out.println("--------");
 		String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-
-		// Printing mortgage value
-		System.out.println("Mortgage: " + mortgageFormatted);
+		System.out.println("Monthly Payments: " + mortgageFormatted);
+		System.out.println();
+		System.out.println("PAYMENT SCHEDULE");
+		System.out.println("----------------");
 
 	}
 
+	public static double readNumber(String prompt, double min, double max) {
+		Scanner sc = new Scanner(System.in);
+		double value;
+		while (true) {
+			System.out.println(prompt);
+			value = sc.nextDouble();
+			if (value >= min && value <= max)
+				break;
+			System.out.println("Enter a number between " + min + " and " + max);
+		}
+
+		return value;
+	}
+
+//	double monthlyInterest = 0;
+
+//	public static double paymentSchedule(int principle, double monthlyInterest, short n, int numberOfPaymentsMade) {
+//		return 1.5;
+//	}
+
 	public static double calculateMortgage(int principle, double interestRate, int period) {
+		// Constants - As months in a year and percentage never change
 		final byte MONTHS_IN_YEAR = 12;
 		final byte PERCENT = 100;
 		// Calculating the monthly interest rate
 		double monthlyInterest = interestRate / PERCENT / MONTHS_IN_YEAR;
 		// Calculating number of payment made over months
-		double n = period * MONTHS_IN_YEAR;
+		short n = (short) (period * MONTHS_IN_YEAR);
 
 		// Mortgage - Displays our monthly payments in currency
 		double mortgage = principle * (monthlyInterest * Math.pow(1 + monthlyInterest, n))
 				/ (Math.pow(1 + monthlyInterest, n) - 1);
 
-		// Formatting the mortgage value into currency so that it is readable, method
-		// chaining instead
-		String mortgageFormatted = (double)NumberFormat.getCurrencyInstance().format(mortgage);
-
+		// Calculating payment schedule
+		int numberOfPaymentsMade = 1;
+		double paymentSchedule = principle * (Math.pow(1 + monthlyInterest, n))
+				- (Math.pow(1 + monthlyInterest, numberOfPaymentsMade)) / (Math.pow(1 + monthlyInterest, n) - 1);
 		
-
-		return mortgageFormatted;
+	return mortgage;
 	}
 
 }
